275f844405185d7036c62df6648b1db67ffd873b
fallinghawks <fallinghawks@gmail.com>
2013-02-03 23:38:00 -0800

    readability; removed lengthy commented-out code

--- src/com/fallinghawks/upgoer/MyTokenizer.java
+++ src/com/fallinghawks/upgoer/MyTokenizer.java
@@ -31,24 +31,6 @@ public class MyTokenizer implements MultiAutoCompleteTextView.Tokenizer {
 
     @Override
     public CharSequence terminateToken(CharSequence text) {
-//        int i = text.length();
-//
-//        while (i > 0 && text.charAt(i - 1) == ' ') {
-//            i--;
-//        }
-//
-//        if (i > 0 && text.charAt(i - 1) == ',') {
-//            return text;
-//        } else {
-//            if (text instanceof Spanned) {
-//                SpannableString sp = new SpannableString(text + ", ");
-//                TextUtils.copySpansFrom((Spanned) text, 0, text.length(),
-//                        Object.class, sp, 0);
-//                return sp;
-//            } else {
-//                return text + ", ";
-//            }
-//        }
         return  text+" ";
     }
 }
